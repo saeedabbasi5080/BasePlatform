73ed70e74b849be01aa2be4d01d91a8b2f2f1b2e
saeid <saeed.abasi5080@gmail.com>
2026-06-05 23:25:17 +0330

    .

---
+++ BasePlatform.Application/Common/Abstractions/IDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace BasePlatform.Application.Common.Abstractions
+{
+    public interface IDispatcher
+    {
+        Task<TResult> SendAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default );
+    }
+}
