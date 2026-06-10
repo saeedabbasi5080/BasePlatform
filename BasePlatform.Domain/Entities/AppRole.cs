f9aa2cff3b16e9d7cf8b59e3c8e321236370fb52
saeid abbasi <saeed.abasi5080@gmail.com>
2026-06-10 18:15:11 +0330

    Phase 4

---
+++ BasePlatform.Domain/Entities/AppRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BasePlatform.Domain.Entities;
+
+public class AppRole : IdentityRole<Guid>
+{
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<RolePermission> RolePermissions { get; set; } = new List<RolePermission>();
+}
---
+++ BasePlatform.Domain/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BasePlatform.Domain.Entities;
+
+public class AppUser : IdentityUser<Guid>
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+
+    public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+}
